4bee32c10277fa83e2323d401c3088c2ad4b1b5a
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-01-11 21:51:08 +0530

    Added shadcn

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/betterIDE"
-})
+  base: "/betterIDE",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
