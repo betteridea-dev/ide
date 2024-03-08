345518f3416ce87a0834cef0bd5824ca04bbfb83
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-03-08 21:25:38 +0530

    Minor changes and shadcn components

--- app_new/src/pages/editor.tsx
+++ app_new/src/pages/editor.tsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Editor, useMonaco } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+
+import theme from "../../themes/code.json";
+import useContracts, { contractsType } from "../../hooks/useContracts";
+
 export default function CEditor() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [searchParams] = useSearchParams();
+
+  const { contracts, setContracts } = useContracts();
+  const [value, setValue] = useState<string | undefined>("");
+
+  const monaco = useMonaco();
+  monaco?.editor.defineTheme("code", theme as editor.IStandaloneThemeData);
+
+  const file = searchParams.get("file")!.split("/");
+  const contractName = file[0];
+  const contractFile = file[1];
+
+  useEffect(() => {
+    if (!contracts) return;
+
+    // console.log(contracts, contractName, contractFile)
+    const c: contractsType = JSON.parse(
+      localStorage.getItem("contracts")! || "{}"
+    );
+    const src = c[contractName][contractFile];
+    setContracts(c);
+    setValue(src);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nc = { ...contracts };
+    nc[contractName][contractFile] = value;
+    setContracts(nc);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
   return (
-    <div>
-      <h1>CEditor</h1>
-    </div>
+    <Editor
+      height="100vh"
+      language={searchParams.get("language")!}
+      theme="code"
+      defaultValue={value}
+      onChange={(value) => {
+        setValue(value);
+      }}
+    />
   );
 }
