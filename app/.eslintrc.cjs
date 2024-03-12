26a016b6a4d381c6c1f2d05b8c11ea8f634a3ace
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-03-12 19:58:05 +0530

    new_app merged

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -14,8 +14,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-undef': 'off',
   },
 }
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,74 @@
-import IDE from "./ide";
-import CEditor from "./editor";
-import { useSearchParams } from "react-router-dom";
-import { ThemeProvider } from "@/components/theme-provider";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+  useSearchParams,
+} from "react-router-dom";
+import IDE from "@/pages/ide";
+import CodeEditor from "@/pages/editor";
+import AONotebookPage from "./pages/ao-notebook";
+import { useCallback, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/store";
+import { setIsWalletConnected } from "./store/app-store";
 
-import "@/styles/globals.css";
+const router = createBrowserRouter([
+  {
+    path: "/notebook",
+    element: <AONotebookPage />,
+  },
+  {
+    path: "/",
+    element: <IDE />,
+  },
+  {
+    path: "/editor",
+    element: <CodeEditor />,
+  },
+]);
 
-export default function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [searchParams, setSearchParams] = useSearchParams();
+function App() {
+  const dispatch = useAppDispatch();
+  const { appMode } = useAppSelector((state) => state.app);
+  const [searchParams] = useSearchParams();
+
+  const _setIsWalletConnected = useCallback(
+    (val: boolean) => {
+      dispatch(setIsWalletConnected(val));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    (async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const wallet = (window as any).arweaveWallet;
+
+      if (!wallet) return alert("Please install the ArConnect extension");
+
+      try {
+        await wallet.getActiveAddress();
+        _setIsWalletConnected(true);
+      } catch (e) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        await wallet.connect(["ACCESS_ADDRESS", "SIGN_TRANSACTION"]);
+        _setIsWalletConnected(true);
+      }
+    })();
+  }, [_setIsWalletConnected]);
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {searchParams.has("editor") ? <CEditor /> : <IDE />}
-    </ThemeProvider>
+    <>
+      {/* <RouterProvider router={router} /> */}
+
+      {searchParams.has("editor") ? (
+        <CodeEditor />
+      ) : appMode === "aos" ? (
+        <AONotebookPage />
+      ) : (
+        <IDE />
+      )}
+    </>
   );
 }
+
+export default App;
