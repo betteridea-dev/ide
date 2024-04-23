b7656c342073696a79f1ba37a0d906d7d6784232
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-04-24 00:08:03 +0530

    Added Toast

--- next_app/src/pages/_app.tsx
+++ next_app/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />;
+    </>
+  );
 }
