c0941482e3a2a241f3645dd3bdcaca5f62a921b7
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-05-08 22:09:05 +0530

    Landing Changes

--- next_app/src/pages/index.tsx
+++ next_app/src/pages/index.tsx
@@ -7,12 +7,11 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function Home() {
   return (
-    // TODO: Test font here
     <main
       className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <section className="container grid grid-cols-2 h-screen p-24 items-center">
-        <div className="flex flex-col gap-6">
+      <section className="container p-24 my-16">
+        <div className="flex flex-col gap-6 items-center">
           <h1 className="text-6xl font-bold">Welcome, {"{pikachu}"}</h1>
 
           <p className="text-lg text-muted-foreground">
@@ -30,26 +29,21 @@ export default function Home() {
       </section>
 
       <section className="container flex flex-col gap-y-20 px-20">
-        <div className="flex flex-col gap-6 border rounded-xl p-4 px-6">
-          <p className="text-lg">Recently opened projects</p>
+        <div className="flex flex-col gap-6 rounded-xl p-4 px-6">
+          <p className="text-lg text-[#494949]">Recently opened projects</p>
 
           <ScrollArea>
-            <div className="flex flex-row text-muted items-center gap-x-4 h-[152px] w-full">
-              <div className="h-full border rounded-2xl aspect-square flex items-center justify-center text-4xl">
-                +
-              </div>
-
-              <div className="h-full border rounded-2xl aspect-[1.5] flex items-center justify-center">
-                Project Name
-              </div>
-              <div className="h-full border rounded-2xl aspect-[1.5] flex items-center justify-center">
-                Project Name
-              </div>
-              <div className="h-full border rounded-2xl aspect-[1.5] flex items-center justify-center">
-                Project Name
-              </div>
-
-              <p className="px-8 underline">See all</p>
+            <div className="flex flex-row text-muted items-center gap-x-4 h-[96px] w-full">
+              {Array(3)
+                .fill(null)
+                .map((_, i) => (
+                  <div
+                    key={i}
+                    className="h-full text-[#707070] bg-[#0D0D0D] border border-[#282828] rounded-2xl aspect-[2.25] flex items-center justify-center"
+                  >
+                    Project Name
+                  </div>
+                ))}
             </div>
 
             <ScrollBar orientation="horizontal" />
@@ -57,21 +51,50 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-2">
-          <h3 className="text-2xl">Templates</h3>
-          <p>
+          <h3 className="text-2xl text-[#707070]">Templates</h3>
+          <p className="text-[#353535]">
             Integrate pre-built and customisable scripts seamlessly into your ao
             projects with minimal coding.
           </p>
 
           <div className="grid grid-cols-2 gap-4 mt-8">
-            <div className="h-[264px] w-full border rounded-xl">
-              Token Managament System
+            <div className="h-[264px] w-full p-8 border rounded-xl bg-[#353535] relative">
+              <h3 className="text-primary text-lg">Token Managament System</h3>
+
+              <Image
+                src="/images/token-mgmt.svg"
+                alt="Token Management System"
+                className="absolute bottom-0 right-0"
+                width={200}
+                height={160}
+              />
             </div>
-            <div className="h-[264px] w-full border rounded-xl">
-              Decentralised Voting Script
+
+            <div className="h-[264px] w-full p-8 border rounded-xl bg-[#353535] relative">
+              <h3 className="text-primary text-lg">
+                Decentralised Voting Script
+              </h3>
+
+              <Image
+                src="/images/voting-script.svg"
+                alt="Token Management System"
+                className="absolute bottom-0 right-8"
+                width={180}
+                height={120}
+              />
             </div>
-            <div className="h-[264px] w-full border rounded-xl col-span-2">
-              Real Time Chat Integration
+            <div className="h-[264px] w-full p-8 border rounded-xl bg-[#353535] relative col-span-2">
+              <h3 className="text-primary text-lg">
+                Real Time Chat Integration
+              </h3>
+
+              <Image
+                src="/images/chat.svg"
+                alt="Token Management System"
+                className="absolute bottom-0 right-0"
+                width={180}
+                height={120}
+              />
             </div>
           </div>
         </div>
@@ -86,6 +109,14 @@ export default function Home() {
           journey!
         </p>
       </section>
+
+      <Image
+        src="/images/footer.svg"
+        alt="Token Management System"
+        className=""
+        width={750}
+        height={500}
+      />
 
       <div className="h-52"></div>
     </main>
