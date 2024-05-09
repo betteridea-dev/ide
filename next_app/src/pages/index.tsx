069084390b8d9261d6d3ef3e3dae02917c94f745
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-05-09 18:35:37 +0530

    Landing

--- next_app/src/pages/index.tsx
+++ next_app/src/pages/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Icons } from "@/components/icons";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -83,6 +85,7 @@ export default function Home() {
                 height={120}
               />
             </div>
+
             <div className="h-[264px] w-full p-8 border rounded-xl bg-[#353535] relative col-span-2">
               <h3 className="text-primary text-lg">
                 Real Time Chat Integration
@@ -98,22 +101,111 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
 
-      <section className="container p-20 text-center flex flex-col mt-20 gap-8 items-center justify-center">
-        <h2 className="text-4xl">Feeling lost?</h2>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl text-[#707070]">Feeling lost?</h3>
+          <p className="text-[#353535]">
+            ao is a new battlefield, and navigating it might feel difficult.
+            Don&apos;t worry, BetterIDEa has some resources that will help you
+            in the journey!
+          </p>
 
-        <p>
-          ao is a new battlefield, and navigating it might feel difficult. Don’t
-          worry, BetterIDEa has some resources that will help you in the
-          journey!
-        </p>
+          <div className="grid grid-cols-2 gap-4 mt-8">
+            <div className="h-[264px] w-full p-8 border rounded-3xl bg-[#121212] relative">
+              <h3 className="text-primary text-2xl font-light">Docs</h3>
+
+              <p className="text-[#353535]">
+                Master ao on BetterIDEa with in-depth documentation
+              </p>
+
+              <Image
+                src="/images/docs.svg"
+                alt="Token Management System"
+                className="absolute bottom-0 right-0"
+                width={110}
+                height={75}
+              />
+            </div>
+
+            <div className="h-[264px] w-full p-8 border rounded-3xl bg-[#121212] flex flex-col justify-end">
+              <h3 className="text-primary text-2xl font-light">Docs</h3>
+
+              <p className="text-[#353535]">
+                Accelerate development with pre-built code
+              </p>
+            </div>
+
+            <div className="h-[264px] p-8 w-full border rounded-3xl bg-[#121212] flex flex-row justify-between items-center gap-4 col-span-2">
+              <div className="h-full">
+                <h3 className="text-primary text-2xl font-light">Learn AO</h3>
+
+                <p className="text-[#353535]">
+                  Explore concepts of ao and practice it hands-on
+                </p>
+              </div>
+
+              <div className="flex flex-col justify-between gap-6">
+                {[
+                  {
+                    title: "Quick Start",
+                    description: "Get started with ao - for complete beginners",
+                    tasks: 5,
+                    buttonText: "Begin Course >",
+                    buttonHref: "/",
+                    icon: (
+                      <Icons.golfFlag
+                        height={42}
+                        width={42}
+                        className="fill-white self-center"
+                      />
+                    ),
+                  },
+                  {
+                    title: "Messaging",
+                    description: "Communicate with process in the ao network",
+                    tasks: 7,
+                    buttonText: "Begin Course >",
+                    buttonHref: "/",
+                    icon: (
+                      <Icons.smiley
+                        height={42}
+                        width={42}
+                        className=" self-center"
+                      />
+                    ),
+                  },
+                ].map((item, i) => (
+                  <div key={i} className="grid gap-4 grid-cols-7 items-center">
+                    <div className="p-4 rounded-md bg-[#222222} border border-[#2e2e2e] flex aspect-square">
+                      {item.icon}
+                    </div>
+
+                    <div className="flex flex-col justify-between gap-2 col-span-3">
+                      <div>
+                        <h4 className="text-lg">{item.title}</h4>
+                        <p className="text-sm">{item.description}</p>
+                      </div>
+
+                      <p className="text-[#5c5c5c]">{item.tasks} tasks</p>
+                    </div>
+
+                    <Link href={item.buttonHref} className="col-span-3 h-full">
+                      <Button className="h-full w-full bg-white border border-black outline-double">
+                        {item.buttonText}
+                      </Button>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <Image
         src="/images/footer.svg"
         alt="Token Management System"
-        className=""
+        className="mt-20"
         width={750}
         height={500}
       />
