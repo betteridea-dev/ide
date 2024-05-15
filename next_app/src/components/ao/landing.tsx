dd7aa17e6b511d5b9722609540c53909e2719ab0
ankushKun <ankush4singh@gmail.com>
2024-05-15 13:03:03 +0530

    random emotes

--- next_app/src/components/ao/landing.tsx
+++ next_app/src/components/ao/landing.tsx
@@ -1,23 +1,45 @@
 
 const words = [
+    "winston",
     "dumdum",
     "pikachu",
     "bulbasaur",
     "charmander",
     "squirtle",
     "jigglypuff",
+    "buildooor",
+    "(^･o･^)ﾉ' ",
+    "ฅ^•ﻌ•^ฅ",
+    "(⁠ ⁠╹⁠▽⁠╹⁠ ⁠)",
+    "(⁠≧⁠▽⁠≦⁠)",
+    "<⁠(⁠￣⁠︶⁠￣⁠)⁠>",
+    "＼⁠(⁠^⁠o⁠^⁠)⁠／",
+    "/⁠ᐠ⁠｡⁠ꞈ⁠｡⁠ᐟ⁠\\",
+    "/⁠╲⁠/⁠\\⁠╭⁠(⁠•⁠‿⁠•⁠)⁠╮⁠/⁠\⁠\╱⁠\\",
+    "▼⁠・⁠ᴥ⁠・⁠▼",
+    "〜⁠(⁠꒪⁠꒳⁠꒪⁠)⁠〜",
+    "(⁠⁠‾⁠▿⁠‾⁠)⁠",
+    "\⁠(⁠ϋ⁠)⁠/⁠♩",
+    "ᕙ⁠(⁠ ⁠ ⁠•⁠ ⁠‿⁠ ⁠•⁠ ⁠ ⁠)⁠ᕗ",
+    "ᕙ⁠(⁠ ͡⁠°⁠ ͜⁠ʖ⁠ ͡⁠°⁠)⁠ᕗ",
+    "⁄⁠(⁠⁄⁠ ⁠⁄⁠•⁠⁄⁠-⁠⁄⁠•⁠⁄⁠ ⁠⁄⁠)⁠⁄"
 ]
+
+const cat = `
+ᓚᘏᗢ
+`
 
 export default function AOLanding() {
     return <>
         <section className="container p-24 my-16">
-            <div className="flex flex-col gap-6 items-center">
-                <h1 className="text-6xl font-bold">Welcome, {
+            <div className="flex flex-col gap-5 items-center">
+
+                <h1 className="text-6xl font-bold" suppressHydrationWarning>gm {
                     words[Math.floor(Math.random() * words.length)]
                 }</h1>
 
                 <p className="text-lg text-muted-foreground">
-                    The intuitive web IDE for building powerful <span className="text-primary">actor oriented</span> applications.
+                    Welcome to the intuitive web IDE for building powerful <span className="text-primary">actor oriented</span> applications.
                 </p>
 
                 <div className="flex flex-col text-center gap-x-4 mt-8">
