1aadb15a585199a4507c54fda654a2f9322b6c00
ankushKun <ankush4singh@gmail.com>
2024-07-10 17:00:12 +0530

    weaveworld agent

--- next_app/src/templates/index.ts
+++ next_app/src/templates/index.ts
@@ -1,11 +1,11 @@
 import { source as aoBot } from "@/templates/ao/ao-bot";
 import { source as arInGrid } from "@/templates/ao/ar-in-arena";
 import { source as memeFrame } from "@/templates/ao/memeframe";
-import {source as LlamaWanderer} from "@/templates/ao/llama-wanderer";
+import {source as LlamaWanderer} from "@/templates/ao/weaveworld-agent";
 
 export const AOTemplates = {
     "": 'print("Hello AO!")',
-    "Llama Wanderer": LlamaWanderer,
+    "WeaveWorld Agent": LlamaWanderer,
     "ArweaveIndia Deathmatch Arena": arInGrid,
     "Deathmatch Bot": aoBot,
     "MemeFrame": memeFrame
