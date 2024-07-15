065fba18b01c184005c2110deddff363c6101b55
ankushKun <ankush4singh@gmail.com>
2024-07-15 18:11:07 +0530

    persist package list

--- next_app/src/components/drawer/components/package-list.tsx
+++ next_app/src/components/drawer/components/package-list.tsx
@@ -6,12 +6,13 @@ import { connect } from "@permaweb/aoconnect";
 import { toast } from "sonner";
 import { LoaderIcon, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useSessionStorage } from "usehooks-ts";
 
 function PackageList() {
     const globalState = useGlobalState();
     const manager = useProjectManager();
     const ao = connect();
-    const [packages, setPackages] = useState<TPackage[]>([]);
+    const [packages, setPackages] = useSessionStorage<TPackage[]>("packages", [], { initializeWithValue: true });
     const [loading, setLoading] = useState(false);
     const [activePackage, setActivePackage] = useState<TPackage | null>(null);
     const [search, setSearch] = useState("");
@@ -24,12 +25,12 @@ function PackageList() {
         setLoading(true);
         const pop = await ao.dryrun({
             process: APM_ID,
-            tags: [ { name: "Action", value: "APM.GetPopular" }],
+            tags: [{ name: "Action", value: "APM.GetPopular" }],
         })
         setLoading(false);
         if (pop.Error) return toast.error("Error fetching popular packages", { description: pop.Error, id: "error" })
         const { Messages } = pop;
-        const msg = Messages.find((msg) => msg.Tags.find((tag:Tag) => tag.name == "Action").value == "APM.GetPopularResponse")
+        const msg = Messages.find((msg) => msg.Tags.find((tag: Tag) => tag.name == "Action").value == "APM.GetPopularResponse")
         if (!msg) return toast.error("Error fetching popular packages", { description: "No popular response found", id: "error" })
         const data = JSON.parse(msg.Data);
         setPackages(data);
@@ -75,12 +76,12 @@ function PackageList() {
             fetchPopular();
         }
     }, [search])
-    
+
     function viewPackage(pkg: TPackage) {
         if (!pkg) return
         setActivePackage(pkg);
-        if (!globalState.openedPackages.find((p) => p.PkgID == pkg.PkgID)) 
-        globalState.addOpenedPackage(pkg);
+        if (!globalState.openedPackages.find((p) => p.PkgID == pkg.PkgID))
+            globalState.addOpenedPackage(pkg);
         globalState.setActiveView("EDITOR");
         globalState.setActiveFile(`PKG: ${pkg.Vendor}/${pkg.Name}`);
     }
@@ -94,11 +95,12 @@ function PackageList() {
             <Input placeholder="Search packages" className="rounded-md border border-border/30 h-fit p-1 pl-6" onChange={(e) => setDebounceVal(e.target.value)} />
         </div>
         <div className="grid grid-cols-1 overflow-scroll p-2 gap-2">
+            {loading && packages.length == 0 && <LoaderIcon className="animate-spin mx-auto" />}
             {
-                loading ? <><LoaderIcon className=" animate-spin mx-auto" /></> :
-                    packages.map((pkg: TPackage, _: number) => {
-                    return <div key={_} data-active={pkg.PkgID == activePackage?.PkgID} className="p-1 rounded-md px-2 border  border-border/30 cursor-pointer data-[active=true]:bg-accent hover:bg-accent/30" onClick={()=>viewPackage(pkg)}>
-                        <div>{ pkg.Vendor!="@apm"&&`${pkg.Vendor}/`}{pkg.Name}</div>
+
+                packages.map((pkg: TPackage, _: number) => {
+                    return <div key={_} data-active={pkg.PkgID == activePackage?.PkgID} className="p-1 rounded-md px-2 border  border-border/30 cursor-pointer data-[active=true]:bg-accent hover:bg-accent/30" onClick={() => viewPackage(pkg)}>
+                        <div>{pkg.Vendor != "@apm" && `${pkg.Vendor}/`}{pkg.Name}</div>
                         <div className="truncate">{pkg.Description}</div>
                         <div className="text-xs text-right">{pkg.Installs} installs</div>
                     </div>
@@ -108,7 +110,7 @@ function PackageList() {
     </div>
 }
 
-const drawerItem:TDrawerItem = {
+const drawerItem: TDrawerItem = {
     component: PackageList,
     label: "Packages",
     value: "PACKAGES"
