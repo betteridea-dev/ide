5d9ff854b52ea847a995da954a5209e8a4d6e34c
ankushKun <ankush4singh@gmail.com>
2024-07-24 12:24:10 +0530

    add: fantasy llama template

---
+++ next_app/src/templates/ao/fantasy-llama.ts
@@ -0,0 +1,44 @@
+export const source = `-- Name: FantasyLlama
+
+CHAT_TARGET = CHAT_TARGET or 'TODO: Put your verse ID here'
+
+-- To add this agent to your verse, configure your Static Entities table, e.g.:
+-- VerseEntitiesStatic = {
+--   ['<your agent process Id>'] = {
+--     Position = { 10, 10 },
+--     Type = 'Avatar',
+--     Metadata = {
+--       DisplayName = 'Fantasy Llama',
+--       SkinNumber = 5,
+--       Interaction = {
+--         Type = 'Default',
+--       },
+--     },
+--   },
+-- }
+
+TIMESTAMP_LAST_MESSAGE_MS = TIMESTAMP_LAST_MESSAGE_MS or 0
+COOLDOWN_MS = 5000
+
+Handlers.add(
+  'DefaultInteraction',
+  Handlers.utils.hasMatchingTag('Action', 'DefaultInteraction'),
+  function(msg)
+    print('DefaultInteraction')
+    if ((msg.Timestamp - TIMESTAMP_LAST_MESSAGE_MS) < COOLDOWN_MS) then
+      return
+    end
+
+    Send({
+      Target = CHAT_TARGET,
+      Tags = {
+        Action = 'ChatMessage',
+        ['Author-Name'] = 'Fantasy Llama',
+      },
+      Data =
+      "Where on earth did you come from? I heard if you take the blue p...otion, you'll be able to return to your world.",
+    })
+
+    TIMESTAMP_LAST_MESSAGE_MS = msg.Timestamp
+  end
+)`
--- next_app/src/templates/index.ts
+++ next_app/src/templates/index.ts
@@ -1,11 +1,13 @@
 import { source as aoBot } from "@/templates/ao/ao-bot";
 import { source as arInGrid } from "@/templates/ao/ar-in-arena";
 import { source as memeFrame } from "@/templates/ao/memeframe";
-import {source as LlamaWanderer} from "@/templates/ao/weaveworld-agent";
+import { source as LlamaWanderer } from "@/templates/ao/weaveworld-agent";
+import { source as FantasyLlama } from "@/templates/ao/fantasy-llama";
 
 export const AOTemplates = {
     "": 'print("Hello AO!")',
     "WeaveWorld Agent": LlamaWanderer,
+    "Fantasy Llama": FantasyLlama,
     "ArweaveIndia Deathmatch Arena": arInGrid,
     "Deathmatch Bot": aoBot,
     "MemeFrame": memeFrame
