c86a5871296eff156df0fe001c2fbf61eec3a77f
ankushKun <ankush4singh@gmail.com>
2024-08-21 10:55:02 +0530

    analytics

--- next_app/backend/index.ts
+++ next_app/backend/index.ts
@@ -48,6 +48,7 @@ const codecell_load_table = `CREATE TABLE IF NOT EXISTS codecell_loads (
     org TEXT,
     as_ TEXT
 )`;
+db.run(codecell_load_table)
 
 const codecell_run_table = `CREATE TABLE IF NOT EXISTS codecell_runs (
     timestamp NUMBER,
@@ -64,6 +65,7 @@ const codecell_run_table = `CREATE TABLE IF NOT EXISTS codecell_runs (
     org TEXT,
     as_ TEXT
 )`;
+db.run(codecell_run_table);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
@@ -71,7 +73,171 @@ app.get('/', (req, res) => {
 
 
 app.get('/analytics', (req, res) => {
-    res.status(200).send('Analytics data');
+    //count codecell_loads
+    // const r = db.all(`
+    //     SELECT COUNT(*) as total_loads FROM codecell_loads;
+    //     SELECT COUNT(*) as total_runs FROM codecell_runs;
+    //     SELECT COUNT(DISTINCT userId) as total_users FROM codecell_loads;
+    //     SELECT COUNT(DISTINCT appName) as total_apps FROM codecell_loads;
+    //     SELECT COUNT(DISTINCT referrer) as total_referrers FROM codecell_loads;
+
+    //     SELECT COUNT(*) as total_loads_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(*) as total_runs_today FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT userId) as total_users_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT appName) as total_apps_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT referrer) as total_referrers_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)};
+
+    //     SELECT COUNT(*) as total_loads_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(*) as total_runs_yesterday FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT userId) as total_users_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT appName) as total_apps_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)};
+    //     SELECT COUNT(DISTINCT referrer) as total_referrers_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)};
+
+    //     SELECT COUNT(*) as total_loads_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 604800000};
+    //     SELECT COUNT(*) as total_runs_lastweek FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 604800000};
+    //     SELECT COUNT(DISTINCT userId) as total_users_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 604800000};
+    //     SELECT COUNT(DISTINCT appName) as total_apps_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 604800000};
+    //     SELECT COUNT(DISTINCT referrer) as total_referrers_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 604800000};
+
+    //     SELECT COUNT(*) as total_loads_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 2592000000};
+    //     SELECT COUNT(*) as total_runs_lastmonth FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 2592000000};
+    //     SELECT COUNT(DISTINCT userId) as total_users_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 2592000000};
+    //     SELECT COUNT(DISTINCT appName) as total_apps_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 2592000000};
+    //     SELECT COUNT(DISTINCT referrer) as total_referrers_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 2592000000};
+    //     `, (err, rows) => {
+    //     if (err) {
+    //         console.log(err)
+    //         res.status(500).send(err);
+    //     } else {
+    //         res.status(200).send(rows);
+    //     }
+    // })
+
+    const queries = [
+        //total
+        `SELECT COUNT(*) as total_loads FROM codecell_loads`,
+        `SELECT COUNT(*) as total_runs FROM codecell_runs`,
+        `SELECT COUNT(DISTINCT userId) as total_users FROM codecell_loads`,
+        `SELECT COUNT(DISTINCT appName) as total_apps FROM codecell_loads`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers FROM codecell_loads`,
+        //today
+        `SELECT COUNT(*) as total_loads_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(*) as total_runs_today FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_today FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0)}`,
+        //yesterday
+        `SELECT COUNT(*) as total_loads_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(*) as total_runs_yesterday FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_yesterday FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000} AND timestamp < ${new Date().setHours(0, 0, 0, 0)}`,
+        // this week
+        `SELECT COUNT(*) as total_loads_thisweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay()}`,
+        `SELECT COUNT(*) as total_runs_thisweek FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay()}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_thisweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay()}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_thisweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay()}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_thisweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay()}`,
+        //previous week
+        `SELECT COUNT(*) as total_loads_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay() - 604800000}`,
+        `SELECT COUNT(*) as total_runs_lastweek FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay() - 604800000}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay() - 604800000}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay() - 604800000}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_lastweek FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDay() - 604800000}`,
+        // this month
+        `SELECT COUNT(*) as total_loads_thismonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate()}`,
+        `SELECT COUNT(*) as total_runs_thismonth FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate()}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_thismonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate()}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_thismonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate()}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_thismonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate()}`,
+        // previous month
+        `SELECT COUNT(*) as total_loads_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate() - 2592000000}`,
+        `SELECT COUNT(*) as total_runs_lastmonth FROM codecell_runs WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate() - 2592000000}`,
+        `SELECT COUNT(DISTINCT userId) as total_users_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate() - 2592000000}`,
+        `SELECT COUNT(DISTINCT appName) as total_apps_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate() - 2592000000}`,
+        `SELECT COUNT(DISTINCT referrer) as total_referrers_lastmonth FROM codecell_loads WHERE timestamp > ${new Date().setHours(0, 0, 0, 0) - 86400000 * (new Date()).getDate() - 2592000000}`,
+
+    ]
+
+    // run all queries 1 by one and collect results
+    const results = []
+    queries.forEach((query) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send(err);
+                return;
+            }
+            results.push(rows);
+            if (results.length === queries.length) {
+                let data = results.map((r) => r[0]).reduce((acc, cur) => ({ ...acc, ...cur }), {})
+                // nest the data according to time groups- today, yesterday, lastweek, lastmonth
+                data = {
+                    total: {
+                        loads: data.total_loads,
+                        runs: data.total_runs,
+                        users: data.total_users,
+                        apps: data.total_apps,
+                        referrers: data.total_referrers
+                    },
+                    today: {
+                        loads: data.total_loads_today,
+                        runs: data.total_runs_today,
+                        users: data.total_users_today,
+                        apps: data.total_apps_today,
+                        referrers: data.total_referrers_today
+                    },
+                    yesterday: {
+                        loads: data.total_loads_yesterday,
+                        runs: data.total_runs_yesterday,
+                        users: data.total_users_yesterday,
+                        apps: data.total_apps_yesterday,
+                        referrers: data.total_referrers_yesterday
+                    },
+                    thisweek: {
+                        loads: data.total_loads_thisweek,
+                        runs: data.total_runs_thisweek,
+                        users: data.total_users_thisweek,
+                        apps: data.total_apps_thisweek,
+                        referrers: data.total_referrers_thisweek
+                    },
+                    lastweek: {
+                        loads: data.total_loads_lastweek,
+                        runs: data.total_runs_lastweek,
+                        users: data.total_users_lastweek,
+                        apps: data.total_apps_lastweek,
+                        referrers: data.total_referrers_lastweek
+                    },
+                    thismonth: {
+                        loads: data.total_loads_thismonth,
+                        runs: data.total_runs_thismonth,
+                        users: data.total_users_thismonth,
+                        apps: data.total_apps_thismonth,
+                        referrers: data.total_referrers_thismonth
+                    },
+                    lastmonth: {
+                        loads: data.total_loads_lastmonth,
+                        runs: data.total_runs_lastmonth,
+                        users: data.total_users_lastmonth,
+                        apps: data.total_apps_lastmonth,
+                        referrers: data.total_referrers_lastmonth
+                    }
+                }
+                res.status(200).send(data);
+            }
+        })
+    })
+})
+
+app.get('/analytics/referrers', (req, res) => {
+    db.all(`SELECT referrer, COUNT(*) as count FROM codecell_loads GROUP BY referrer ORDER BY count DESC`, (err, rows) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(rows);
+        }
+    })
 })
 
 app.post('/analytics', async (req, res) => {
@@ -103,7 +269,6 @@ app.post('/analytics', async (req, res) => {
 
     switch (body.action) {
         case "codecell_load":
-            db.run(codecell_load_table)
             await (new Promise((resolve, reject) => { setTimeout(resolve, 100) }))
             db.prepare(`
                 INSERT INTO codecell_loads (timestamp, userId, appName, referrer, country, countryCode, region, regionName, city, isp, org, as_)
@@ -111,7 +276,6 @@ app.post('/analytics', async (req, res) => {
                 `).run(date.getTime(), body.userId, body.appName, body.referrer, body.geo.country, body.geo.countryCode, body.geo.region, body.geo.regionName, body.geo.city, body.geo.isp, body.geo.org, body.geo.as)
             break;
         case "codecell_run":
-            db.run(codecell_run_table);
             await (new Promise((resolve, reject) => { setTimeout(resolve, 100) }))
             db.prepare(`
                 INSERT INTO codecell_runs (timestamp, userId, appName, messageId, referrer, country, countryCode, region, regionName, city, isp, org, as_)
