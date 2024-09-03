b7ba3ab46dcdb76dfa842a3ddebaa17f874c73fe
ankushKun <ankush4singh@gmail.com>
2024-09-03 11:35:49 +0530

    fix

--- next_app/src/pages/bounties.tsx
+++ next_app/src/pages/bounties.tsx
@@ -1,7 +1,10 @@
-import Layout from "@/components/layout";
+import { useEffect } from "react";
 
 export default function Bounties() {
-    window.location.href = "https://betteridea.dev/bounties";
+    useEffect(() => {
+        window.location.href = "https://betteridea.dev/bounties";
+
+    }, [])
 
     return <main className="flex items-center text-center justify-center w-screen h-screen">
         <div>Loading Bounties... <span className="inline-block animate-spin">⌛️</span></div>
