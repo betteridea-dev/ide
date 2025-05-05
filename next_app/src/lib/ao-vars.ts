466c5f2d6fa5f6beeee166c2d264446ecd449dda
ankushKun <ankush4singh@gmail.com>
2025-05-05 18:54:43 +0530

    add tailwind css

--- next_app/src/lib/ao-vars.ts
+++ next_app/src/lib/ao-vars.ts
@@ -111,7 +111,7 @@ const processHtml = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <script type="module">
         import { aofetch } from "https://unpkg.com/ao-fetch@latest/dist/browser.mjs";
 
@@ -162,7 +162,7 @@ const processHtml = `<!DOCTYPE html>
             const body = document.querySelector("body");
             body.innerHTML = "<h1>aofetch error</h1>";
         }
-    </script>
+    </title>
 </head>
 
 <body></body>
